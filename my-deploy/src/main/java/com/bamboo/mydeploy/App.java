f81a42d85e90795131072a9bd5ae29e7f312ce7b
Deniz Timartas Tree <deniztimartas@gmail.com>
2018-08-07 13:20:29 +0300

    The initial Maven project was built

---
+++ my-deploy/src/main/java/com/bamboo/mydeploy/App.java
@@ -0,0 +1,13 @@
+package com.bamboo.mydeploy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
